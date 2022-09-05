969927210e3ba9d9e353aaad0ae2b5bfdccbe477
Rodrigo Martins de Souza <rmartinsdesouza@gmail.com>
2022-09-05 14:27:04 -0300

    Aulas: 1 instalação python e VS code, 2 entrada de dados input e 3 input do dados com type

---
+++ primeiro.py
@@ -0,0 +1,4 @@
+print('Hello World!')
+print('Seja bem vind(o)')
+
+input('Qual e a liguagem de programação que você esta aprendendo?')
---
+++ variaveis.py
@@ -0,0 +1,13 @@
+idade = 26
+
+print(idade)
+
+nome = 'Rodrigo Martins de Souza'
+
+print(type(nome))
+
+print(type(idade))
+
+liguagem = input('Qual é liguagem de progração que você esta estudando?')
+
+print('A liguagem que você esta estudando é :', liguagem)
